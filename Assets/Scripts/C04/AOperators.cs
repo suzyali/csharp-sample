eb3b1799f8e38644fe252823e8499f214844ebe2
suzy <si.jogg72@skkukdp.re.kr>
2023-11-28 11:25:27 +0900

    코드 작성 완료
    코드를 작성 완료했습니다. / 파일 이름 변경.

---
+++ Assets/Scripts/C04/AOperators.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AOperators : MonoBehaviour 
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a = 111 * 222;
+        Debug.Log($"a : {a}");
+
+   
+    }
+
+
+}
